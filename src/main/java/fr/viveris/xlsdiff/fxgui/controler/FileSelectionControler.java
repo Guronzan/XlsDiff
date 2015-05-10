099e2a98924864c2ab8f0b38dea09b5979b28973
Guillaume RODRIGUES <guillaume.rodrigues@gmail.com>
2015-05-10 22:32:20 +0200

    Adding ant & launch4j to build (still some fixes to perform to launch4j...) Adding a check on non valid file selection

--- src/main/java/fr/viveris/xlsdiff/fxgui/controler/FileSelectionControler.java
+++ src/main/java/fr/viveris/xlsdiff/fxgui/controler/FileSelectionControler.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
@@ -15,68 +17,82 @@ import fr.viveris.xlsdiff.fxgui.MainWindow;
 
 @Slf4j
 public class FileSelectionControler {
-	@FXML
-	private Button buttonCancel;
-	@FXML
-	private Button buttonRunCompare;
-	@FXML
-	private Button buttonFile1;
-	@FXML
-	private Button buttonFile2;
-	@FXML
-	private TextField fieldFile1;
-	@FXML
-	private TextField fieldFile2;
+    @FXML
+    private Button buttonCancel;
+    @FXML
+    private Button buttonRunCompare;
+    @FXML
+    private Button buttonFile1;
+    @FXML
+    private Button buttonFile2;
+    @FXML
+    private TextField fieldFile1;
+    @FXML
+    private TextField fieldFile2;
 
-	private final MainWindow mainInstance = MainWindow.getInstance();
-	private FXMLLoader mainFxmlLoader;
+    private final MainWindow mainInstance = MainWindow.getInstance();
+    private FXMLLoader mainFxmlLoader;
 
-	public void initialize() {
-		this.buttonRunCompare.setOnAction(event -> {
-			run();
+    public void initialize() {
+        this.buttonRunCompare.setOnAction(event -> {
+            run();
 
-		});
+        });
 
-		this.buttonCancel.setOnAction(event -> {
-			this.mainInstance.getFileSelectionStage().close();
-		});
+        this.buttonCancel.setOnAction(event -> {
+            this.mainInstance.getFileSelectionStage().close();
+        });
 
-		this.buttonFile1.setOnAction(event -> {
-			final FileChooser fileChooser = new FileChooser();
-			fileChooser.setTitle("Select File 1");
-			final File file1 = fileChooser.showOpenDialog(this.mainInstance
-					.getFileSelectionStage());
-			this.fieldFile1.setText(file1.getAbsolutePath());
-		});
-		this.buttonFile2.setOnAction(event -> {
-			final FileChooser fileChooser = new FileChooser();
-			fileChooser.setTitle("Select File 2");
-			final File file2 = fileChooser.showOpenDialog(this.mainInstance
-					.getFileSelectionStage());
-			this.fieldFile2.setText(file2.getAbsolutePath());
-		});
-		this.mainFxmlLoader = new FXMLLoader(
-				MainWindow.class.getResource("/mainFrame.fxml"));
-		try {
-			final Parent page = this.mainFxmlLoader.load();
-			final Stage primaryStage = this.mainInstance.getPrimaryStage();
-			final Scene scene = new Scene(page);
-			primaryStage.setScene(scene);
-		} catch (final Exception e) {
-			log.error("Error while opening MainWindow", e);
-		}
-	}
+        this.buttonFile1.setOnAction(event -> {
+            final FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Select File 1");
+            final File file1 = fileChooser.showOpenDialog(this.mainInstance
+                    .getFileSelectionStage());
+            this.fieldFile1.setText(file1.getAbsolutePath());
+        });
+        this.buttonFile2.setOnAction(event -> {
+            final FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Select File 2");
+            final File file2 = fileChooser.showOpenDialog(this.mainInstance
+                    .getFileSelectionStage());
+            this.fieldFile2.setText(file2.getAbsolutePath());
+        });
+        this.mainFxmlLoader = new FXMLLoader(
+                MainWindow.class.getResource("/mainFrame.fxml"));
+        try {
+            final Parent page = this.mainFxmlLoader.load();
+            final Stage primaryStage = this.mainInstance.getPrimaryStage();
+            final Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+        } catch (final Exception e) {
+            log.error("Error while opening MainWindow", e);
+        }
+    }
 
-	private void run() {
-		final Stage primaryStage = this.mainInstance.getPrimaryStage();
-		primaryStage.show();
-		this.mainInstance.getFileSelectionStage().close();
-		final SheetListControler sheetListControler = this.mainFxmlLoader
-				.getController();
+    private void run() {
+        // TODO ajouter vérification d'existence et de non nullicité des chemins
+        // saisis
+        final File file1 = new File(this.fieldFile1.getText());
+        final File file2 = new File(this.fieldFile2.getText());
+        final Stage fileSelectionStage = this.mainInstance
+                .getFileSelectionStage();
+        if (file1.exists() && file2.exists()) {
+            final Stage primaryStage = this.mainInstance.getPrimaryStage();
 
-		// TODO ajouter vérification d'existence et de non nullicité des chemins
-		// saisis
-		sheetListControler.process(this.fieldFile1.getText(),
-				this.fieldFile2.getText());
-	}
+            primaryStage.show();
+            fileSelectionStage.close();
+            final SheetListControler sheetListControler = this.mainFxmlLoader
+                    .getController();
+
+            sheetListControler.process(this.fieldFile1.getText(),
+                    this.fieldFile2.getText());
+        } else {
+            final Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Invalid files");
+            alert.setHeaderText(null);
+            alert.setContentText("At least one of the selected files doesn't exist, please check their existence before launch compare");
+
+            alert.showAndWait();
+        }
+    }
 }
